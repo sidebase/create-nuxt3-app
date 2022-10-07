c137bde81edc17c8fbb5c1987391182f9f59bef2
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-10-07 16:20:36 +0200

    Added mergepackagejson plop action

---
+++ packages/cn3a-plop-files/helpers.js
@@ -0,0 +1,19 @@
+export function combinePackageJson(primaryJSON, secondaryJSON) {
+  const fileData = primaryJSON;
+
+  // Add dependencies
+  Object.keys(secondaryJSON.dependencies).forEach((packageName) => {
+    if (fileData.dependencies[name]) {
+      return;
+    }
+    fileData.dependencies[name] = secondaryJSON.dependencies[packageName]
+  })
+  // Add devDependencies
+  Object.keys(secondaryJSON.devDependencies).forEach((packageName) => {
+    if (fileData.devDependencies[name]) {
+      return;
+    }
+    fileData.devDependencies[name] = secondaryJSON.devDependencies[packageName]
+  })
+  return fileData
+}
--- packages/cn3a-plop-files/plopfile.js
+++ packages/cn3a-plop-files/plopfile.js
@@ -1,5 +1,6 @@
 const { sidebase } = require('./plops');
 const fs = require('fs-extra');
+const {combinePackageJson} = require("./helpers");
 
 module.exports = function (plop) {
   // Create custom Plop Action to copy files without parsing them
@@ -7,6 +8,19 @@ module.exports = function (plop) {
     const srcDir = plop.renderString(config.src, answers)
     const destDir = plop.renderString(config.dest, answers)
     fs.copySync(srcDir, destDir, { overwrite: false })
+  })
+
+  // Create custom Plop action to merge two package.json files
+  plop.setActionType('mergePackageJSON', (answers, config, plop) => {
+      fs.readJson(config.mainJSONFile, {}, (err, fileData) => {
+        fs.readJson(config.secondJSONFile, {}, (err, secondFileData) => {
+          const json = combinePackageJson(fileData, secondFileData)
+
+          fs.writeJson(config.JSONFile, json, {spaces: 2}, err => {
+            console.error(err);
+          });
+        })
+      });
   })
 
   // Add generators
